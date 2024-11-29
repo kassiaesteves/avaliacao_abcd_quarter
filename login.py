348eae993e1bba8aa690fd1a47ff540b904f9c6c
ana-zoom <ana.marianni@zoomtecnologia.com.br>
2024-11-29 12:00:47 -0300

    update link base

--- login.py
+++ login.py
@@ -13,7 +13,7 @@ DB_HTTP_PATH = os.getenv("DB_HTTP_PATH")
 DB_ACCESS_TOKEN = os.getenv("DB_ACCESS_TOKEN")
 
 # Chave secreta para gerar o token JWT
-#secret_key = "data"
+secret_key = "data"
 
 def conectar_banco():
     return sql.connect(
@@ -35,7 +35,7 @@ def verificar_login(username, password):
     connection.close()
     return resultado['id_emp'] if resultado else None
 
-#def gerar_token(user_id):
+def gerar_token(user_id):
     token = jwt.encode(
         {
             "user_id": user_id,
@@ -46,7 +46,7 @@ def verificar_login(username, password):
     )
     return token
 
-#def salvar_token_no_banco(user_id, token):
+def salvar_token_no_banco(user_id, token):
     connection = conectar_banco()
     cursor = connection.cursor()
     cursor.execute(f"""
@@ -70,11 +70,11 @@ def login_page():
         if login_button:
             id_emp = verificar_login(username, password)
             if id_emp:
-                #token = gerar_token(id_emp)
-                #salvar_token_no_banco(id_emp, token)
+                token = gerar_token(id_emp)
+                salvar_token_no_banco(id_emp, token)
                 st.session_state["logged_in"] = True
                 st.session_state["id_emp"] = id_emp
-                #st.session_state["token"] = token
+                st.session_state["token"] = token
                 hide_pages([])
                 st.success("Login bem-sucedido! Você será redirecionado.")
                 sleep(0.5)
