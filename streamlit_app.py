a5df42e91756341b809cd1b4bfdf6f850647b106
ana-zoom <ana.marianni@zoomtecnologia.com.br>
2024-11-29 15:04:19 -0300

    redirect app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from alter_nota import func_data_nota
 from st_pages import hide_pages
 import urllib.parse
 
-#link_abcd_base = "https://avaliacaoabcd-quarter.streamlit.app"
-link_abcd_base = "https://aplicacao.streamlit.app"
+link_abcd_base = "https://avaliacao-abcd.streamlit.app"
+#link_abcd_base = "https://aplicacao.streamlit.app"
 
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
