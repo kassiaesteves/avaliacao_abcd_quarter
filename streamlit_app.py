f61e230bd732e6e6fa85867bfbe1196242b18d8e
analyticszoomtecnologia <analyticsdatazoom@gmail.com>
2024-12-23 16:03:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,32 @@
 import streamlit as st
 from login import login_page
+from abcd import abcd_page
 from func_data import func_data_page
 from alter_nota import func_data_nota
+
 from st_pages import hide_pages
-import urllib.parse
 
-link_abcd_base = "https://avaliacao-abcd.streamlit.app"
-#link_abcd_base = "https://aplicacao.streamlit.app"
-
+# Verifica se o usuário está logado
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
+# Se o usuário não estiver logado, mostra a página de login
 if not st.session_state['logged_in']:
-    hide_pages(["Avaliação ABCD", "Funcionários Data", "Lista de Avaliados"])
+    hide_pages(["Avaliação ABCD", "Funcionários Data", "Lista de Avaliados"])  # Oculta as páginas
     login_page()
 else:
-    hide_pages([])
+    hide_pages([])  # Mostra todas as páginas
 
+    # Seletor de páginas na barra lateral
     st.sidebar.title("Navegação")
     pagina_selecionada = st.sidebar.selectbox(
         "Escolha a página",
         ["Avaliação ABCD", "Funcionários Data", "Lista de Avaliados"]
     )
 
+    # Navega para a página selecionada
     if pagina_selecionada == "Avaliação ABCD":
-        user_id = st.session_state["id_emp"]
-        link_abcd = f"{link_abcd_base}?user_id={urllib.parse.quote(str(user_id))}"
-
-        #st.write("Redirecionando para a página principal...")
-        st.markdown(f"[Clique aqui para Realizar Avaliação.]({link_abcd})", unsafe_allow_html=True)
-
+        abcd_page()
     elif pagina_selecionada == "Funcionários Data":
         func_data_page()
     elif pagina_selecionada == "Lista de Avaliados":
